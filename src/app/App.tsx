b2ba68788def53d1ea37bd34cfa5bf27711fc2ad
Kod-dasm <boxwar2009@gmail.com>
2024-06-02 15:40:03 +0500

    nt-1 Добавил компонент отображающий видео

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,19 @@
+import { VideoModel } from '../components'
 import './App.scss'
 
 export const App = () => {
 
   return (
     <div className='app'>
-      Netris Test
+      <div className='app__title'>
+        <h2 className='app__title--gray'>
+          Netris Test
+        </h2>
+        <h1>
+          BIG BUCK BUNNY
+        </h1>
+      </div>
+      <VideoModel />
     </div>
   )
 }
