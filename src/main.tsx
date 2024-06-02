3022ef4792b22050f1006c561aa4332469561f7a
Kod-dasm <boxwar2009@gmail.com>
2024-06-03 00:58:18 +0500

    nt-2 Добавил кастомный контроллер для компонента VideoModel

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import { store } from './store';
+// import { store } from './store';
 import { App } from './app';
 import React from 'react';
 import ReactDom from 'react-dom/client';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 
 import './index.scss'
 
